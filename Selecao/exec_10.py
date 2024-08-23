2f64b6165fc73356bb1d0105edbc1268c7b59234
Matheus Macedo <matheus.joaonf@gmail.com>
2024-08-23 18:27:11 -0300

    aula 10

---
+++ Selecao/exec_10.py
@@ -0,0 +1,14 @@
+#Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+
+
+#Entrada
+valor1 = int(input ("Digite o primeiro valor")) # 5 
+valor2 = int(input ("Digite o segundo valor")) # 2
+valor3 = int(input ("Digite o terceiro valor")) # 7
+
+
+#Processamento
+
+
+
+#Saída

44f43cf45efefc12ca894b8bd0edbc014e6e4dc7
Matheus Macedo <matheus.joaonf@gmail.com>
2024-08-23 18:57:31 -0300

    exercicio 10, aula09

--- Selecao/exec_10.py
+++ Selecao/exec_10.py
@@ -2,13 +2,29 @@
 
 
 #Entrada
-valor1 = int(input ("Digite o primeiro valor")) # 5 
-valor2 = int(input ("Digite o segundo valor")) # 2
-valor3 = int(input ("Digite o terceiro valor")) # 7
+valor1 = int(input ("Digite o primeiro valor?")) # 5 
+valor2 = int(input ("Digite o segundo valor?")) # 2
+valor3 = int(input ("Digite o terceiro valor?")) # 7
 
 
 #Processamento
+if (valor1 < valor2 and valor2 < valor3 ):
+    resposta = valor1, "<", valor2 , "<" , valor3
 
+elif (valor1< valor3 and valor3< valor2 ):
+    resposta = valor1 , "<" , valor3 , "<" , valor2
 
+elif (valor2< valor3 and valor3< valor1 ):
+    resposta = valor2 , "<" , valor3 , "<" , valor1
+
+elif (valor2 < valor1 and valor1 < valor3 ):
+    resposta = valor2 , "<" , valor1 , "<" , valor3        
+
+elif (valor3< valor1 and valor1< valor2 ):
+    resposta = valor3 , "<" , valor1 , "<" , valor2
+
+else: #(valor3 < valor2 and valor2   < valor 3):
+    resposta = valor3 , "<" , valor2 , "<" , valor3
 
 #Saída
+print (resposta)
